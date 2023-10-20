1ee1e5a1f077567c905af61357455891e86368a7
ConanProZs <colinden09@gmail.com>
2023-10-20 16:10:31 +0200

    Initial commit

---
+++ src/main/java/com/conaitus/conanstuff/ConanStuffClient.java
@@ -0,0 +1,11 @@
+package com.conaitus.conanstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConanStuffClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
